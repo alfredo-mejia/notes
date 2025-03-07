7eb813bb1d72123fd2e2b436bc79ad3d1a3675fd
Alfredo Mejia <alfredomejia.dev@gmail.com>
2025-03-07 09:09:00 -0600

    added server to use for book exercises

---
+++ JavaScript_and_JQuery_Interactive_Frontend_Web_Development/014.My_Book_Project/server/src/main/java/io/github/alfredomejia/jsbookserver/ServerApplication.java
@@ -0,0 +1,26 @@
+package io.github.alfredomejia.jsbookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @GetMapping("/ch1/1")
+    public String ch1_1(@RequestParam(value = "name", defaultValue = "") String name) {
+        if (name.isEmpty()) {
+            return "Server cannot process empty name";
+        }
+        System.out.println("Received name: " + name);
+        return "Received name: " + name;
+    }
+}
