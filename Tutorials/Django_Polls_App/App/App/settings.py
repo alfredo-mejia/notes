26b3adbe7c44b361570679e95dfdf7d7c52836ab
Alfredo Mejia <alfredomejia.dev@gmail.com>
2025-03-24 20:47:47 -0500

    in-progess django tutorial

--- Tutorials/Django_Polls_App/App/App/settings.py
+++ Tutorials/Django_Polls_App/App/App/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+
+import polls
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,11 +34,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "polls.apps.PollsConfig",
 ]
 
 MIDDLEWARE = [
